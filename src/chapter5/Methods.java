bd95560a9efc2728642441306d6e1c0d5f1ab4bc
Edwin Leonardo Quintero Castaño <equinteroc@choucairtesting.com>
2022-04-04 23:12:49 -0500

    métodos

--- src/chapter5/Methods.java
+++ src/chapter5/Methods.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 //escriba un método que le pregunte a un usuario su nombre, luego lo salude por su nombre
 public class Methods {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese su nombre: ");
+        String nombre = scanner.next();
+        saludarPorNombre(nombre);
     }
 
-    public void saludarPorNombre(String nombre, String apellido) {
-        Scanner scanner = new Scanner(System.in);
+    public static void saludarPorNombre(String nombre) {
+        System.out.println("Hola " + nombre);
 
     }
 }
